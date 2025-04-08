76955a4bedcdc277997515bc028bf50660452933
thly112 <thaolythly@gmail.com>
2025-04-09 00:29:00 +0700

    start page

---
+++ app/src/main/java/com/example/oderapp/Splash_Screen.java
@@ -0,0 +1,25 @@
+package com.example.oderapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splash_Screen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash_Screen.this, StartActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}
