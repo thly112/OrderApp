22577669683a619700ae37aa51de80c5f05018d6
tranmaidi <ditran.120804@gmail.com>
2025-04-09 23:03:01 +0700

    trang home, load banner quang cao

--- app/src/main/java/com/example/oderapp/StartActivity.java
+++ app/src/main/java/com/example/oderapp/StartActivity.java
@@ -1,6 +1,9 @@
 package com.example.oderapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,8 +16,16 @@ public class StartActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_start);
 
+        Button btnStarted = findViewById(R.id.startBtn);
+        btnStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Chuyển sang MainActivity
+                Intent intent = new Intent(StartActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
